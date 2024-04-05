ffc95377693db731bc69f32af3e262d83ab10d21
AshTechs <ashbelh@gmail.com>
2024-04-04 22:31:51 -0700

    Good

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,31 +2,31 @@
 """Fabric script to create and distribute an archive to web servers"""
 
 from fabric.api import env, local, run
-from os.path import exists
 from datetime import datetime
 
-env.hosts = ['<IP web-01>', '<IP web-02>']
+env.hosts = ['34.204.61.94', '54.242.154.151']
 
 
 def do_pack():
-    """Creates a compressed archive of web_static folder"""
+    """Creates a compressed archive of web_static"""
     try:
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        filename = 'versions/web_static_{}.tgz'.format(timestamp)
+        current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        file_name = 'versions/web_static_{}.tgz'.format(current_time)
         local('mkdir -p versions')
-        local('tar -czvf {} web_static'.format(filename))
-        return filename
-    except Exception:
+        local('tar -cvzf {} web_static'.format(file_name))
+        return file_name
+    except:
         return None
 
 
 def do_deploy(archive_path):
     """Distributes an archive to web servers"""
-    if not exists(archive_path):
+    if not archive_path:
         return False
 
     try:
         # Upload the archive to /tmp/ directory
+        run('mkdir -p /tmp/')
         put(archive_path, '/tmp/')
 
         # Get archive filename without extension
@@ -44,7 +44,9 @@ def do_deploy(archive_path):
         run('rm /tmp/{}'.format(archive_filename))
 
         # Update symbolic link
-        update_symlink(archive_folder)
+        current_link = '/data/web_static/current'
+        run('rm -f {}'.format(current_link))  # Remove old link
+        run('ln -s {} {}'.format(archive_folder, current_link)) #Create new link
 
         return True
 
@@ -53,17 +55,9 @@ def do_deploy(archive_path):
         return False
 
 
-def update_symlink(archive_folder):
-    """Updates the symbolic link"""
-    current_link = '/data/web_static/current'
-    run('rm -f {}'.format(current_link))  # Remove old link
-    run('ln -s {} {}'.format(archive_folder, current_link))  # Create new link
-
-
 def deploy():
-    """Creates and distributes an archive to web servers"""
+    """Deploys web_static"""
     archive_path = do_pack()
     if not archive_path:
         return False
-
     return do_deploy(archive_path)
