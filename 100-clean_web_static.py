f8c75917049a519ac4afd56f3e6e9ca0d650e277
AshTechs <ashbelh@gmail.com>
2024-04-07 19:46:49 -0700

    Good

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,103 +1,24 @@
 #!/usr/bin/python3
-"""Fabric script to clean up outdated archives"""
+"""Fabric script to delete out-of-date archives"""
 
-from fabric.api import env, local, run
+from fabric.api import env, run, local, lcd
 from datetime import datetime
-import os
-
+from os.path import exists
 env.hosts = ['<IP web-01>', '<IP web-02>']
-env.user = 'ubuntu'
 
 
 def do_clean(number=0):
-    """Cleans up outdated archives"""
+    """Deletes out-of-date archives"""
     number = int(number)
     if number < 1:
         number = 1
-    else:
-        number += 1
-
-    # Clean local archives
-    local("cd versions; ls -t | tail -n +{} | xargs rm -f".format(number))
-
-    # Clean remote archives
-    run("cd /data/web_static/releases; ls -t | tail -n +{} | xargs rm -rf"
-        .format(number))
-
-
-def do_pack():
-    """Creates a compressed archive of web_static folder"""
     try:
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        filename = 'versions/web_static_{}.tgz'.format(timestamp)
-        local('mkdir -p versions')
-        local('tar -czvf {} web_static'.format(filename))
-        return filename
-    except Exception:
-        return None
+        # Local cleanup
+        with lcd('versions'):
+            local('ls -t | tail -n +{} | xargs rm -f'.format(number + 1))
 
-
-def deploy():
-    """Creates and distributes an archive to web servers"""
-    archive_path = do_pack()
-    if not archive_path:
-        return False
-
-    return do_deploy(archive_path)
-
-
-def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
-    if not os.path.exists(archive_path):
-        return False
-
-    try:
-        # Upload the archive to /tmp/ directory
-        put(archive_path, '/tmp/')
-
-        # Get archive filename without extension
-        archive_filename = archive_path.split('/')[-1]
-        archive_folder = '/data/web_static/releases/' + \
-            archive_filename.split('.')[0]
-
-        # Create directory to uncompress archive
-        run('mkdir -p {}'.format(archive_folder))
-
-        # Uncompress the archive
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename,
-                                            archive_folder))
-
-        # Delete the archive
-        run('rm /tmp/{}'.format(archive_filename))
-
-        # Update symbolic link
-        update_symlink(archive_folder)
-
-        return True
+        # Remote cleanup
+        run('ls -t /data/web_static/releases | tail -n +{} | xargs rm -rf'.format(number + 1))
 
     except Exception as e:
         print(e)
-        return False
-
-
-def update_symlink(archive_folder):
-    """Updates the symbolic link"""
-    current_link = '/data/web_static/current'
-    run('rm -f {}'.format(current_link))  # Remove old link
-    run('ln -s {} {}'.format(archive_folder, current_link))  # Create new link
-
-
-def do_clean(number=0):
-    """Cleans up outdated archives"""
-    number = int(number)
-    if number < 1:
-        number = 1
-    else:
-        number += 1
-
-    # Clean local archives
-    local("cd versions; ls -t | tail -n +{} | xargs rm -f".format(number))
-
-    # Clean remote archives
-    run("cd /data/web_static/releases; ls -t | tail -n +{} | xargs rm -rf"
-        .format(number))
