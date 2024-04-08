638801a585365f13cd42c7074d666866487f4e7d
AshTechs <ashbelh@gmail.com>
2024-04-07 17:24:30 -0700

    Good

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,33 +3,29 @@
 
 # Install Nginx if it's not already installed
 if ! command -v nginx &>/dev/null; then
-    apt-get update
-    apt-get -y install nginx
+    sudo apt-get update
+    sudo apt-get -y install nginx
 fi
 
 # Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 
 # Create a fake HTML file for testing Nginx configuration
-echo "<html><head></head><body>Test Page</body></html>" > /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Test Page</body></html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
 
 # Create symbolic link /data/web_static/current
 if [[ -L /data/web_static/current ]]; then
-    rm /data/web_static/current
+    sudo rm /data/web_static/current
 fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of /data/ folder to ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
 config_file="/etc/nginx/sites-available/default"
-sed -i '/^server {/a \
-    location /hbnb_static/ {\
-        alias /data/web_static/current/;\
-    }\
-' "$config_file"
+sudo sed -i '/^server {/a \    location /hbnb_static/ {\        alias /data/web_static/current/;\    }\ ' "$config_file"
 
 # Restart Nginx
-service nginx restart
+sudo service nginx restart
