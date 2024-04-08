1495c4733dab5318636647bca1611c432a494039
AshTechs <ashbelh@gmail.com>
2024-04-07 18:20:59 -0700

    Good

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""
-Fabric script to create and distribute an archive to web servers
-"""
+"""Fabric script to create and distribute an archive to web servers"""
 
-from fabric.api import env, run, local
+from fabric.api import env, local, run
 from os.path import exists
 from datetime import datetime
 
-env.hosts = ['34.204.61.94', '54.242.154.151']
+env.hosts = ['<IP web-01>', '<IP web-02>']
 
 
 def do_pack():
-    """
-    Compresses web_static folder into .tgz format
-    """
+    """Creates a compressed archive of web_static folder"""
     try:
-        now = datetime.now()
-        timestamp = now.strftime("%Y%m%d%H%M%S")
-        local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(filename))
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        filename = 'versions/web_static_{}.tgz'.format(timestamp)
+        local('mkdir -p versions')
+        local('tar -czvf {} web_static'.format(filename))
         return filename
     except Exception:
         return None
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers
-    """
+    """Distributes an archive to web servers"""
     if not exists(archive_path):
         return False
 
@@ -60,19 +53,16 @@ def do_deploy(archive_path):
 
 
 def update_symlink(archive_folder):
-    """
-    Updates the symbolic link
-    """
+    """Updates the symbolic link"""
     current_link = '/data/web_static/current'
     run('rm -f {}'.format(current_link))  # Remove old link
     run('ln -s {} {}'.format(archive_folder, current_link))  # Create new link
 
 
 def deploy():
-    """
-    Deploys the archive to web servers
-    """
+    """Creates and distributes an archive to web servers"""
     archive_path = do_pack()
     if not archive_path:
         return False
+
     return do_deploy(archive_path)
