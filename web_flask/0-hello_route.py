20cbf8c729e82f87873de82ceffa83d63b982c06
AshTechs <ashbelh@gmail.com>
2024-04-21 09:39:33 -0700

    Good

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-
-""" Start a flask with application. """
-
-
+""" Starts a Flash Web Application """
 from flask import Flask
-
 app = Flask(__name__)
 
 
-# Define the route for the root URL '/'
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Display 'Hello HBNB! """
-    return "hello HBNB!"
-
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    # Start the flask development server
-    # Listen on all available network interfaces (0.0.0.0) and port 500
-    app.run(host='0.0.0.0', port=500)
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
